4326c777e50848bdaf754edaddc8ef40861b0c21
dlwlals1289 <dlwlals1289@gmail.com>
2025-08-20 12:48:55 +0900

    ⚙  Init    : springboot app docker에서 삭제"

---
+++ ddabong/src/main/java/com/hana7/ddabong/DdabongApplication.java
@@ -0,0 +1,14 @@
+package com.hana7.ddabong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdabongApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdabongApplication.class, args);
+	}
+
+}
