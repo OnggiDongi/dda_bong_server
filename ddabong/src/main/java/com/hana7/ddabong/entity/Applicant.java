b745dbd74079229c252332b16adfb98df8b57179
bokyeommmmm <bokyeommmmm@github.com>
2025-08-22 10:39:37 +0900

    📌 Feat    : 어노테이션 추가 (Applicant)

--- ddabong/src/main/java/com/hana7/ddabong/entity/Applicant.java
+++ ddabong/src/main/java/com/hana7/ddabong/entity/Applicant.java
@@ -17,9 +17,19 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+@EqualsAndHashCode(callSuper = true)
 @Entity
+@Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class Applicant extends BaseEntity{
 
 	@Id
