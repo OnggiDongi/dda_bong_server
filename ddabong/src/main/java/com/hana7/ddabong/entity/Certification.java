5d0a422cde6c1f3f4ecc17a01ed7f6a2ea53470c
bokyeommmmm <bokyeommmmm@github.com>
2025-08-22 13:12:25 +0900

    📌 Feat    : Certification Entity 생성

---
+++ ddabong/src/main/java/com/hana7/ddabong/entity/Certification.java
@@ -0,0 +1,50 @@
+package com.hana7.ddabong.entity;
+
+import org.hibernate.annotations.ColumnDefault;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Entity
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Certification extends BaseEntity{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "hour", nullable = false)
+	private int hour;
+
+	// 일반유저
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(
+		name = "user_id",
+		foreignKey = @ForeignKey(
+			name = "fk_Likes_User",
+			foreignKeyDefinition = """
+					foreign key (user_id)
+					   references User(id)
+					    on DELETE cascade on UPDATE cascade
+				"""
+		)
+	)
+	@ToString.Exclude
+	private User user;
+}
