631e0ad840af3eda2657a4f026e7a200382b0ec0
bokyeommmmm <bokyeommmmm@github.com>
2025-08-26 14:21:02 +0900

    📝 Modify  : review 이미지 nullable, review validation

--- ddabong/src/main/java/com/hana7/ddabong/dto/ActivityReviewRequestDTO.java
+++ ddabong/src/main/java/com/hana7/ddabong/dto/ActivityReviewRequestDTO.java
@@ -1,10 +1,21 @@
 package com.hana7.ddabong.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class ActivityReviewRequestDTO {
+
+    @NotBlank(message = "평점을 입력해주세요.")
+    @Min(value = 1, message = "평점은 최소 1점이어야 합니다.")
+    @Max(value = 5, message = "평점은 최대 5점이어야 합니다.")
     private int rate;
+
+    @NotBlank(message = "내용을 입력해주세요.")
     private String content;
+
     private String imageUrl;
 }
