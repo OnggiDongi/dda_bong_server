7358f8b7b6403253014ef93372ebb5f833cbda1c
dlwlals1289 <dlwlals1289@gmail.com>
2025-08-26 14:34:08 +0900

    ❗️ Fix     : ERRORCODE 중복된 코드 정리 & errorCode 정리

--- ddabong/src/main/java/com/hana7/ddabong/enums/ErrorCode.java
+++ ddabong/src/main/java/com/hana7/ddabong/enums/ErrorCode.java
@@ -6,23 +6,18 @@ import lombok.Getter;
 public enum ErrorCode {
 	NOTFOUND_USER(101, "해당 아이디를 가진 회원을 찾을 수 없습니다."),
 	NOTFOUND_INSTITUTION(102, "해당 아이디를 가진 기관을 찾을 수 없습니다."),
-	NOTFOUND_ACTIVITY(102, "해당 아이디를 가진 봉사활동을 찾을 수 없습니다."),
+	NOTFOUND_ACTIVITY(103, "해당 아이디를 가진 봉사활동을 찾을 수 없습니다."),
+	NOTFOUND_REVIEW(104, "해당 리뷰를 찾을 수 없습니다."),
+	NOTFOUND_ACTIVITY_POST(105, "해당 활동 게시글을 찾을 수 없습니다."),
+	NOTFOUND_CERTIFICATION(106, "해당 인증서를 찾을 수 없습니다."),
 
 	BAD_REQUEST_FUTURE_BIRTHDATE(201, "생년월일이 현재보다 미래일 수 없습니다."),
-	BAD_REQUEST_UNAUTHORIZED(201, "권한이 없습니다."),
-
-	NOTFOUND_REVIEW(103, "해당 리뷰를 찾을 수 없습니다."),
-	NOTFOUND_ACTIVITY_POST(104, "해당 활동 게시글을 찾을 수 없습니다."),
-	NOTFOUND_CERTIFICATION(105, "해당 인증서를 찾을 수 없습니다."),
-
-
-	BAD_REQUEST_FUTURE_BIRTHDATE(201, "생년월일이 현재보다 미래일 수 없습니다."),
-	BAD_REQUEST_INSTITUTION_MISMATCH(202, "리뷰를 작성하려는 활동의 주최 기관이 아닙니다."),
-	BAD_REQUEST_NOT_APPLICANT(203, "해당 활동에 참여한 회원이 아닙니다."),
-	BAD_REQUEST_NO_PERMISSION(204, "해당 작업을 수행할 권한이 없습니다."),
-	BAD_REQUEST_CERTIFICATION_ACCESS_DENIED(205, "해당 인증서를 조회할 권한이 없습니다."),
-	BAD_REQUEST_ACTIVITY_NOT_COMPLETED(206, "아직 완료되지 않은 활동입니다."),
-
+	BAD_REQUEST_UNAUTHORIZED(202, "권한이 없습니다."),
+	BAD_REQUEST_INSTITUTION_MISMATCH(203, "리뷰를 작성하려는 활동의 주최 기관이 아닙니다."),
+	BAD_REQUEST_NOT_APPLICANT(204, "해당 활동에 참여한 회원이 아닙니다."),
+	BAD_REQUEST_NO_PERMISSION(205, "해당 작업을 수행할 권한이 없습니다."),
+	BAD_REQUEST_CERTIFICATION_ACCESS_DENIED(206, "해당 인증서를 조회할 권한이 없습니다."),
+	BAD_REQUEST_ACTIVITY_NOT_COMPLETED(207, "아직 완료되지 않은 활동입니다."),
 
 	CONFLICT_USER(301, "이미 존재하는 회원입니다."),
 	CONFLICT_INSTITUTION(302, "이미 존재하는 기관입니다.");
