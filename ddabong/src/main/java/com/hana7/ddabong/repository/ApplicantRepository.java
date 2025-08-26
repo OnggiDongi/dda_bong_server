b69ea13c4cdf5cceaa85d02123e64ccbd691cb3e
bokyeommmmm <bokyeommmmm@github.com>
2025-08-26 10:07:44 +0900

    📌 Feat    : 회원 리뷰 CRD

--- ddabong/src/main/java/com/hana7/ddabong/repository/ApplicantRepository.java
+++ ddabong/src/main/java/com/hana7/ddabong/repository/ApplicantRepository.java
@@ -8,4 +8,6 @@ import java.util.List;
 
 public interface ApplicantRepository extends JpaRepository<Applicant, Long> {
     List<Applicant> findByUser(User user);
+
+    boolean existsByUserAndActivityPostId(User user, Long activityPostId);
 }
